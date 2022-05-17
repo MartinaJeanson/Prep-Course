8f921b38b4b7428d42894a9975c56999c54de335
MartinaJeanson <martinajeanson1996@gmail.com>
2022-05-16 23:49:13 -0300

    tarea corregida

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -222,7 +222,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra === a || letra === e || letra === i || letra === o || letra === u || ) {
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ) {
     return 'es vocal'
   } 
   if (letra.length !== 1) {
